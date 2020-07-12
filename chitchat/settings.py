4e90fbf6cc2eab7fb417ec7da331de79db21f2e3
bhagirath-radadiya <59503020+bhagirath-radadiya@users.noreply.github.com>
2020-07-12 06:11:03 -0400

    Update settings.py

--- chitchat/settings.py
+++ chitchat/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e_!ac%8_t67t*2q9lwsy^jgi!_jga2n%c)awa7emsfa0wxac#&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
